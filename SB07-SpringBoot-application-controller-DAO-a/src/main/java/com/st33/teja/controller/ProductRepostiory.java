5490ee894cb4fa867c2f5613020fb0059c61d14e
Kodamanchili Charan Teja <152499032+Teja3376@users.noreply.github.com>
2025-03-13 15:45:11 +0530

    Delete SB07-SpringBoot-application-controller-DAO-a directory

--- SB07-SpringBoot-application-controller-DAO-a/src/main/java/com/st33/teja/controller/ProductRepostiory.java
+++
@@ -1,9 +0,0 @@
-package com.st33.teja.controller;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepostiory extends CrudRepository<ProductsBean, Integer> {
-
-}
